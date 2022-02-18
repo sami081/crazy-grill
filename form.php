d956d1662944fc98a403dfde4e93f7819166b6ac
sami081 <75616920+sami081@users.noreply.github.com>
2022-02-18 22:19:38 +0100

    added function mail

--- form.php
+++ form.php
@@ -1,44 +1,24 @@
 <?php
-/* Page: contact.php */
-//mettez ici votre adresse mail
-$VotreAdresseMail="makhloufisami0811@gmail.com";
-// si le bouton "Envoyer" est cliqué
-if(isset($_POST['envoyer'])) {
-    //on vérifie que le champ mail est correctement rempli
-    if(empty($_POST['mail'])) {
-        echo "Le champ mail est vide";
-    } else {
-        //on vérifie que l'adresse est correcte
-        if(!preg_match("#^[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?@[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?\.[a-z]{2,}$#i",$_POST['mail'])){
-            echo "L'adresse mail entrée est incorrecte";
-        }else{
-            //on vérifie que le champ sujet est correctement rempli
-            if(empty($_POST['sujet'])) {
-                echo "Le champ sujet est vide";
-            }else{
-                //on vérifie que le champ message n'est pas vide
-                if(empty($_POST['message'])) {
-                    echo "Le champ message est vide";
-                }else{
-                    //tout est correctement renseigné, on envoi le mail
-                    //on renseigne les entêtes de la fonction mail de PHP
-                    $Entetes = "MIME-Version: 1.0\r\n";
-                    $Entetes .= "Content-type: text/html; charset=UTF-8\r\n";
-                    $Entetes .= "From: Nom de votre site <".$_POST['mail'].">\r\n";//de préférence une adresse avec le même domaine de là où, vous utilisez ce code, cela permet un envoie quasi certain jusqu'au destinataire
-                    $Entetes .= "Reply-To: Nom de votre site <".$_POST['mail'].">\r\n";
-                    //on prépare les champs:
-                    $Mail=$_POST['mail']; 
-                    $Sujet='=?UTF-8?B?'.base64_encode($_POST['sujet']).'?=';//Cet encodage (base64_encode) est fait pour permettre aux informations binaires d'être manipulées par les systèmes qui ne gèrent pas correctement les 8 bits (=?UTF-8?B? est une norme afin de transmettre correctement les caractères de la chaine)
-                    $Message=htmlentities($_POST['message'],ENT_QUOTES,"UTF-8");//htmlentities() converti tous les accents en entités HTML, ENT_QUOTES Convertit en + les guillemets doubles et les guillemets simples, en entités HTML
-                    //en fin, on envoi le mail
-                    if(mail($VotreAdresseMail,$Sujet,nl2br($Message),$Entetes)){//la fonction nl2br permet de conserver les sauts de ligne et la fonction base64_encode de conserver les accents dans le titre
-                        echo "Le mail à été envoyé avec succès!";
-                    } else {
-                        echo "Une erreur est survenue, le mail n'a pas été envoyé";
-                    }
-                }
-            }
-        }
+if (isset($_POST['valider']))
+{
+if(isset($_POST['email']) AND isset($_POST['lastname']) AND isset($_POST['firstname']) AND isset($_POST['reservation']) AND isset($_POST['hour']) AND isset($_POST['telNo'])){
+    if(!empty($_POST['email']) AND !empty($_POST['lastname']) AND !empty($_POST['firstname']) AND !empty($_POST['reservation']) AND !empty($_POST['telNo']))
+    {
+        $email=htmlspecialchars($_POST['email']);
+        $lastName=htmlspecialchars($_POST['lastname']);
+        $firstName=htmlspecialchars($_POST['fistname']);
+        $reservation=htmlspecialchars($_POST['reservation']);
+        $hour=htmlspecialchars($_POST['hour']);
+        $tel=htmlspecialchars($_POST['telNo']);
+        $text=htmlspecialchars($_POST['message']);
+        $message1= "Votre réservation a bien été enregistré à la date de $reservation à $hour au nom de $lastName $firstName. numéro de téléphone : $tel. email: $email";
+        $message2 = wordwrap($message1, 70, "\r\n");
+
+        mail('makhloufisami0811@gmail.com', 'Reservation', $message2);
+     
+        
+
     }
+}
 }
 ?>
