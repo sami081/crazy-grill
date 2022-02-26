aae6cd0e55bd26b0f368c55cd18ff6f7247ebbf6
sami081 <75616920+sami081@users.noreply.github.com>
2022-02-26 14:36:31 +0100

    added function js

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-let menu = document.getElementById("menu");
-console.log(menu);
-menu.addEventListener("mouseover", function(e){
-  e.innerHtml =`<h1> La </h1>`})
+let h = document.querySelector("h3")
+console.log(h);
+let n = document.querySelector(".nav")
+let a = document.querySelector(".a")
+console.log(a);
+
+let createNav = function(){
+  n.innerHTML = ` <a href="menu.html#entree" title="Voir les entree"> entree</a>
+  <a href="menu.html#plats" title="Voir le menu"> plats</a>
+  <a href="menu.html#dessert" title="Voir le menu"> dessert</a>`
+  a.classList.remove("a")
+  a.classList.add("b")
+
+};
+
+let delNav = function(){
+  n.innerHTML= ""
+  a.classList.remove("b")
+  a.classList.add("a")
+}
+h.addEventListener("mouseenter", createNav)
+h.addEventListener("mouseleave", delNav)
