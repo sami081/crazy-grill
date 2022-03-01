f850cad4958e5c565123698e01ad0b5b6fdcae1e
sami081 <75616920+sami081@users.noreply.github.com>
2022-03-02 00:39:59 +0100

    ff

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,13 +1,47 @@
-let firstName
-let lastName
-let adress
-let email
-let dateReservation
-let hourReservation
-let numberCommand=Math.round(Math.random()*10000000)
-const lastNames = document.querySelector("#lastName")
-const firstNames = document.querySelector("firstName")
-const address = document.querySelector("adress")
-const tel = document.querySelector("telNo")
-const mail = document.querySelector("#email")
-console.log(mail);
+// let start;
+
+// const starts = document.querySelector("#start");
+// const form = document.querySelector("form")
+
+// starts.addEventListener("input", (e) => {
+//   start = e.target.value;
+// });
+// console.log(start);
+
+// form.addEventListener("submit",(e)=>{
+//   e.preventDefault();
+
+// })
+
+//afficher date
+
+
+    var cejour = new Date();
+    var options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "2-digit",
+     };
+    var date = cejour.toLocaleDateString("fr-FR", options);
+    // var heure =
+    //   ("0" + cejour.getHours()).slice(-2) +
+    //   ":" +
+    //   ("0" + cejour.getMinutes()).slice(-2) +
+    //   ":" +
+    //   ("0" + cejour.getSeconds()).slice(-2);
+    var dateheure = date 
+    // var dateheure = dateheure.replace(/(^\w{1})|(\s+\w{1})/g, lettre => lettre.toUpperCase());
+    document.getElementById("date").innerHTML = dateheure;
+    // if (
+    //   cejour.getHours() == 23 &&
+    //   cejour.getMinutes() == 53 &&
+    //   cejour.getSeconds() == 00
+    // ) {
+    //   var audio = new Audio("son.mp3");
+    //   audio.play();
+    // }
+//   }
+// }
+// afficherDate();
+
